0ce2e41f8b8e678d8528a577de8aad07463c6361
luckshana03 <lucshana03@gmail.com>
2025-12-25 23:31:55 +0530

    Assessment 1: Without loop - Using goto print weather the number is prime or not

---
+++ Assessment_4/Problem_17.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, i = 2, flag = 1;
+    scanf("%d", &n);
+    if (n <= 1) {
+        printf("Not Prime");
+        return 0;
+    }
+check:
+    if (i <= n / 2) {
+        if (n % i == 0) {
+            flag = 0;
+            goto end;
+        }
+        i++;
+        goto check;
+    }
+end:
+    if (flag == 1)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+}
