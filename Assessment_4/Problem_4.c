34a3c0659b8036d74d5e9802970af119e72ab9dc
luckshana03 <lucshana03@gmail.com>
2025-12-25 23:14:21 +0530

    Assessment 1: Without loop - Using goto print sum of 6 to 1

---
+++ Assessment_4/Problem_4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int x=1,sum=0;
+    loop: if(x>=1 && x<=6){
+        int temp = sum;
+        int temp2 = x;
+        sum = temp + temp2;
+        x++;
+        goto loop;
+    }
+    printf("%d",sum);
+}
+
