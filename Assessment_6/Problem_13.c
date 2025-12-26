1c0cd199e456878a13b1abaf787482202d141cff
luckshana03 <lucshana03@gmail.com>
2025-12-26 22:42:58 +0530

    Assessment 6: Print the reverse of the number

---
+++ Assessment_6/Problem_13.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n, rev = 0;
+    scanf("%ld", &n);
+    while (n > 0) {
+        rev = (rev * 10) + (n % 10);
+        n = n / 10;
+    }
+    printf("%ld", rev);
+}
