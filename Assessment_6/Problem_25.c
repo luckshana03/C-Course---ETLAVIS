fe8d8701f05f63dab4d4c19f8be99b968d7710d3
luckshana03 <lucshana03@gmail.com>
2025-12-26 23:14:53 +0530

    Assessment 5: Print the total number of single digit prime numbers in the number

---
+++ Assessment_6/Problem_25.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n, d, count = 0;
+    scanf("%d", &n);
+    while (n > 0) {
+        d = n % 10;
+        if (d == 2 || d == 3 || d == 5 || d == 7)
+            count++;
+        n = n / 10;
+    }
+    printf("%d", count);
+}
