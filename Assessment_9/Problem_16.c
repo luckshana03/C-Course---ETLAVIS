ecca7742665c69b8fdf00e5a2c9b996ad19d0061
luckshana03 <lucshana03@gmail.com>
2025-12-27 23:36:56 +0530

    Assessment 9: Get multiple numbers from the user and store them in an array. Stop getting numbers when the number is 0. Add each number's digit and put them in a new array and arrange thenew array in as

---
+++ Assessment_9/Problem_16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void add_big_arrays()
+{
+    int a[50], b[50], c[51];
+    int i, carry = 0;
+    for(i = 0; i < 50; i++)
+        scanf("%d", &a[i]);
+    for(i = 0; i < 50; i++)
+        scanf("%d", &b[i]);
+    for(i = 0; i < 51; i++)
+        c[i] = 0;
+    for(i = 49; i >= 0; i--)
+    {
+        int sum = a[i] + b[i] + carry;
+        c[i+1] = sum % 10;
+        carry = sum / 10;
+    }
+    c[0] = carry;
+    for(i = 0; i < 51; i++)
+        printf("%d", c[i]);
+}
+
+int main()
+{
+    add_big_arrays();
+    return 0;
+}
