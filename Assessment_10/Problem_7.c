7347775bcaef5fdf5cfbed12f1b6e2bc1bbc367f
luckshana03 <lucshana03@gmail.com>
2025-12-28 17:01:33 +0530

    Assessment 10: Get a string and find the length of the string

---
+++ Assessment_10/Problem_7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i = 0, length = 0;
+    printf("Enter a string: ");
+    gets(str);   
+    while (str[i] != '\0') {
+        length++;
+        i++;
+    }
+    printf("Length of the string = %d", length);
+    return 0;
+}
