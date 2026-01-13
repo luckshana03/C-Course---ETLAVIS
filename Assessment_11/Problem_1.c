14f4b9124f59b7e98c781c15036bc7f2f6bafb17
luckshana03 <lucshana03@gmail.com>
2026-01-13 23:23:58 +0530

    Assessment 11: Get a number from and print the same. change the value without directly changing it and printthe same

--- Assessment_11/Problem_1.c
+++ Assessment_11/Problem_1.c
@@ -3,16 +3,10 @@
 int main() {
     int num;
     int *ptr;
-
     scanf("%d", &num);
-
     printf("%d\n", num);
-
     ptr = &num;
-
     *ptr = *ptr + 10;  
-
     printf("%d\n", num);
-
     return 0;
 }
